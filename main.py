c31cef41518fa79098f72f8c889c0c7bf7993b69
Massimo Sandretti <massimo.sandretti@gmail.com>
2022-02-16 06:30:29 +0100

    implemented zoom - created utils file

--- main.py
+++ main.py
@@ -1,84 +1,13 @@
 # TODO
-# mst button and implementation
 # zoom implementation
 # selection and multiple node dragging implementation
 
-from re import L
 import pygame
-import random 
+from utils import *
 from pygame.locals import *
 from node import Node
 from button import Button
 from popup import Popup
-
-def mst(graph):    
-    v = len(graph)
-
-    selected = [False]*v
-
-    n_arcs = 0
-
-    selected[0] = True
-    
-    to_keep = []
-
-    while (n_arcs<v-1):
-        minimun = float("inf")
-        x=None
-        y=None
-        for i in range(v):
-            if selected[i]:
-                for arc in graph[i].arcs:
-                    n,w = arc[:2]
-                    if not selected[graph.index(n)]:
-                        if minimun > w:
-                            minimun = w
-                            x = graph[i]
-                            y = n
-        if y != None:
-            to_keep.append((x,y))
-            selected[graph.index(y)] = True
-        n_arcs+=1
-    
-        
-
-    for a in graph:
-        for b in graph:
-            if a != b:
-                if (a,b) not in to_keep and (b,a) not in to_keep:
-                    a.remove_arc(b,True)
-
-
-
-def find_path(starting_node, ending_node,graph):
-    distances = [float("inf")]*len(obj_list)
-    compute_distance(starting_node, distances, graph)
-
-    p = [ending_node]
-    current_node = ending_node
-    while distances[graph.index(current_node)] > 0:
-        for arc in current_node.arcs:
-            n,w = arc[:2]
-            if distances[graph.index(n)] == distances[graph.index(current_node)] - w:
-                current_node = n
-                p.append(n)
-                break # i only consider ONE of the possible shortest paths
-    return p
-
-
-
-def compute_distance(starting_node, results, graph):
-
-    queue = [(starting_node, 0)]
-
-    while len(queue) > 0:
-        current_node, distance = queue.pop(0)
-        if results[graph.index(current_node)] > distance:
-            results[graph.index(current_node)] = distance
-            for n in current_node.arcs:
-                if distance + n[1] < results[graph.index(n[0])]:
-                    queue.append((n[0], distance + n[1]))
-            
 
 CIRCLE_RADIUS = 15
 
@@ -292,9 +221,33 @@ while True:
                     x = Node(mouse_x, mouse_y, CIRCLE_RADIUS)
                     obj_list.append(x)
 
-                    
-                # 5 -> scroll up
-                # 6 -> scroll down
+                elif event.button == 4:
+                    for node in obj_list:
+                        x,y = node.get_position()
+                        mx,my = pygame.mouse.get_pos()
+                        comp_x = x-mx
+                        comp_y = y-my
+                        comp_x=int(comp_x*0.9)
+                        comp_y=int(comp_y*0.9)
+                        new_x,new_y = mx+comp_x,my+comp_y
+                        x,y = x-new_x,y-new_y
+                        node.translate(x,y)
+
+                elif event.button == 5:
+                    for node in obj_list:
+                        x,y = node.get_position()
+                        comp_x = x-width//2
+                        comp_y = y-height//2
+                        comp_x=int(comp_x*1.09)
+                        comp_y=int(comp_y*1.09)
+                        new_x,new_y = width//2+comp_x,height//2+comp_y
+                        x,y = x-new_x,y-new_y
+                        node.translate(x,y)
+
+                
+
+
+              
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     
@@ -411,4 +364,3 @@ while True:
     pygame.display.update()
     
     clock.tick(60)
- 
---
+++ utils.py
@@ -0,0 +1,68 @@
+def mst(graph):    
+    v = len(graph)
+
+    selected = [False]*v
+
+    n_arcs = 0
+
+    selected[0] = True
+    
+    to_keep = []
+
+    while (n_arcs<v-1):
+        minimun = float("inf")
+        x=None
+        y=None
+        for i in range(v):
+            if selected[i]:
+                for arc in graph[i].arcs:
+                    n,w = arc[:2]
+                    if not selected[graph.index(n)]:
+                        if minimun > w:
+                            minimun = w
+                            x = graph[i]
+                            y = n
+        if y != None:
+            to_keep.append((x,y))
+            selected[graph.index(y)] = True
+        n_arcs+=1
+    
+        
+
+    for a in graph:
+        for b in graph:
+            if a != b:
+                if (a,b) not in to_keep and (b,a) not in to_keep:
+                    a.remove_arc(b,True)
+
+
+
+def find_path(starting_node, ending_node,graph):
+    distances = [float("inf")]*len(obj_list)
+    compute_distance(starting_node, distances, graph)
+
+    p = [ending_node]
+    current_node = ending_node
+    while distances[graph.index(current_node)] > 0:
+        for arc in current_node.arcs:
+            n,w = arc[:2]
+            if distances[graph.index(n)] == distances[graph.index(current_node)] - w:
+                current_node = n
+                p.append(n)
+                break # i only consider ONE of the possible shortest paths
+    return p
+
+
+
+def compute_distance(starting_node, results, graph):
+
+    queue = [(starting_node, 0)]
+
+    while len(queue) > 0:
+        current_node, distance = queue.pop(0)
+        if results[graph.index(current_node)] > distance:
+            results[graph.index(current_node)] = distance
+            for n in current_node.arcs:
+                if distance + n[1] < results[graph.index(n[0])]:
+                    queue.append((n[0], distance + n[1]))
+            
